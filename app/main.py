8a39209b6c836dfac66ca122dc6cd3190bd8974c
Arjun Jangid <arjunjangid@Arjuns-MacBook-Air.local>
2026-03-29 14:02:09 +0530

    fix nltk data

--- app/main.py
+++ app/main.py
@@ -3,6 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 from database.connection import engine
 from database.models import Base
+import nltk
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
 
 
 app = FastAPI(
@@ -16,7 +27,7 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     # react frontend
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
