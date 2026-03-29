2961f74fcbef05f179139afaa960d3ec8ce35fa2
Arjun Jangid <arjunjangid@Arjuns-MacBook-Air.local>
2026-03-29 19:36:34 +0530

    Fix DATABASE_URL issue

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./sentiment.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    url=DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
