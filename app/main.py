ea56146d49def1251de98d83647d0d214fb0d149
Arjun Jangid <arjunjangid@Arjuns-MacBook-Air.local>
2026-03-29 14:27:03 +0530

    fix nltk path properly

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import os
 
 nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 os.makedirs(nltk_data_path, exist_ok=True)
-nltk.data.path.append(nltk_data_path)
+nltk.data.path = [nltk_data_path]
 
 nltk.download('stopwords', download_dir=nltk_data_path)
 nltk.download('punkt', download_dir=nltk_data_path)
